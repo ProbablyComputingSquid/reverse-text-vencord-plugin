f6e4508dbb1b00cef34bb3c1337c4e53ab945e6e
Jacob <66024281+ProbablyComputingSquid@users.noreply.github.com>
2024-12-26 20:33:44 -0600

    true reverse + more edge cases

--- reverseText.jsx
+++ reverseText.jsx
@@ -37,7 +37,7 @@ export default definePlugin({
                 message: message,
                 channel: ChannelStore.getChannel(message.channel_id),
                 onClick: async () => {
-                    const reversedText = message.content.split("").reverse().join("");
+                    const reversedText = this.transformText(message.content);
                     updateMessage(message.channel_id, message.id, { content: reversedText });
                 }
             };
@@ -47,10 +47,10 @@ export default definePlugin({
         removePreSendListener(this.preSend);
     },
     transformText(text: string) {
-        const regex = /(:\w+:|<@\d+>|<@!\d+>)/g;
+        const regex = /(:\w+:|<@\d+>|<@!\d+>|https?:\/\/\S+|\[[^\]]+\]\([^)]+\)|[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{2B50}\u{2B55}\u{2934}\u{2935}\u{3030}\u{3297}\u{3299}])/gu;
         const parts = text.split(regex);
 
-        const reversedParts = parts.map(part => {
+        const reversedParts = parts.reverse().map(part => {
             if (regex.test(part)) {
                 return part;
             }
