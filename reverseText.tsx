9c2c6caf47203399fa8dacfb790bfb0afd697a99
Jacob <66024281+ProbablyComputingSquid@users.noreply.github.com>
2024-12-29 15:32:27 -0600

    Update reverseText.tsx

--- reverseText.tsx
+++ reverseText.tsx
@@ -1,13 +1,13 @@
 /*
  * Vencord, a Discord client mod
- * Copyright (c) 2023 Vendicated and contributors
+ * Copyright (c) 2024 Vendicated and contributors
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
 // Needed header for all plugins
 
 import { addPreSendListener, removePreSendListener } from "@api/MessageEvents";
-import { addButton } from "@api/MessagePopover";
+import { addButton, removeButton } from "@api/MessagePopover";
 import { updateMessage } from "@api/MessageUpdater";
 import { definePluginSettings } from "@api/Settings";
 import { classes } from "@utils/misc";
@@ -69,9 +69,9 @@ export default definePlugin({
         this.preSend = addPreSendListener((_channelId, msg) => {
             msg.content = this.transformText(msg.content);
         });
-        addButton("Reverse Text", message => {
+        addButton("vc-reverse", message => {
             return {
-                label: "reversed text",
+                label: "reverse text",
                 icon: ReverseIcon,
                 message: message,
                 channel: ChannelStore.getChannel(message.channel_id),
@@ -84,10 +84,11 @@ export default definePlugin({
     },
     stop() {
         removePreSendListener(this.preSend);
+        removeButton("vc-reverse");
     },
     transformText(text: string) {
         // regex matches emojis, mentions, links, markdown links, and unicode emojis
-        const regex = /(:\w+:|<@\d+>|<@!\d+>|https?:\/\/\S+|\[[^\]]+\]\([^)]+\)|[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{2B50}\u{2B55}\u{2934}\u{2935}\u{3030}\u{3297}\u{3299}])/gu;
+        const regex = /(:\w+:|<@\d+>|<@!\d+>|<#\d+>|https?:\/\/\S+|\[[^\]]+\]\([^)]+\)|[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}\u{2B50}\u{2B55}\u{2934}\u{2935}\u{3030}\u{3297}\u{3299}])/gu;
         const parts = text.split(regex);
 
         const reversedParts = parts.reverse().map(part => {
